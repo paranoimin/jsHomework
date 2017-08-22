c11b3ae98cc9c67f7fd88d00b54046648c4801e3
Jamin <paranoimin@gmail.com>
2017-08-22 21:18:06 +0900

    homework done

--- hw01/src/js/index.js
+++ hw01/src/js/index.js
@@ -1,9 +1,27 @@
 window.onload = function() {
     var trigger = document.getElementById("colorBtn");
-
     trigger.addEventListener("click", bgChange);
 };
 
 function bgChange() {
-    alert("ok");
+    var boxes = document.getElementsByClassName("box");
+
+    var i = 0;
+
+    for (i; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = "#" + colorCode();
+    }
+
+}
+
+function colorCode() {
+
+    var color = "";
+    var code = "abcde0123456789";
+
+    for (var i = 0; i < 6; i++) {
+        color += code.charAt(Math.floor(Math.random() * code.length));
+    }
+
+    return color;
 }
